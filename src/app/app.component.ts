0342f272c180db8b42b9d71bb580cf8bed4ce54c
Andrzej Zaborski <andrzejzab@gmail.com>
2020-01-12 21:41:49 +0100

    Added new commponent products list and array of new Product objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-inventory-app';
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        'NICEHAT',
+        'A nice cowboy hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Man', 'Accessories', 'Hats',],
+        13.99
+      ),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        239.99
+      ),
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        99.99
+      )
+    ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log(`Product clicked ${product}`);
+  }
+
 }
